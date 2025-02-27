23e962e445476bdef6ad444cae80b705eee9c1bb
Douglas Garcia Rangel <douglasgovmad@gmail.com>
2025-02-27 03:06:15 -0300

    removendo recaptcha para testar

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    emailjs.init("NIYDrK5we5-T6fQjH"); // Inicia o EmailJS com sua Public Key
+    emailjs.init("NIYDrK5we5-T6fQjH");
 
-    // Adiciona o evento de submit no formulário
     document.getElementById("contact-form").addEventListener("submit", function (event) {
-        event.preventDefault();  // Impede o envio padrão do formulário
-
-        // Captura o token do reCAPTCHA
-        var captchaResponse = grecaptcha.getResponse();
-        if (!captchaResponse) {
-            alert("Por favor, complete o reCAPTCHA!"); // Exibe um alerta se o reCAPTCHA não foi completado
-            return;
-        }
+        event.preventDefault();
 
         // Dados do formulário
         var params = {
             name: document.getElementById("name").value,
             email: document.getElementById("email").value,
             message: document.getElementById("message").value,
-            "g-recaptcha-response": captchaResponse, // Envia o token do reCAPTCHA
+            // Não enviar mais o campo "g-recaptcha-response"
         };
 
         // Envia o e-mail pelo EmailJS
@@ -25,7 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(function (response) {
                 alert("Mensagem enviada com sucesso!");
                 document.getElementById("contact-form").reset();
-                grecaptcha.reset(); // Reseta o reCAPTCHA após o envio
             }, function (error) {
                 alert("Erro ao enviar mensagem, tente novamente!");
                 console.error("Erro:", error);
